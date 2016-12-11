12b28ff511390680b69a3ece96493bbaebcaa375
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 21:49:57 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,20 +1,2 @@
-import microgear.client as microgear
-import time
-microgear.create(<gearkey>,<gearsecret>, <appid>,{'debugmode': True})
-def connection():
-      print("Now I am connected with netpie")
-def subscription(topic,message):
-      print(topic+" "+message)
-def disconnect():
-      print("disconnected")
-microgear.setalias("Raspberry Pi Python")
-microgear.on_connect = connection
-microgear.on_message = subscription
-microgear.on_disconnect = disconnect
-microgear.subscribe("/token")
-microgear.subscribe("/refresh")
-microgear.subscribe("/settingStandard")
-microgear.subscribe("/settingDetails")
-microgear.subscribe("/controlDevieces")
-microgear.subscribe("/alarm")
-microgear.connect()
+def captureImage():
+      os.system("fswebcam -r 1280x720 --no-banner image/garden.jpg")
