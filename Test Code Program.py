0f0319a80cd8692e5a7b4c910127ad523ed32279
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 11:51:02 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,9 +1,7 @@
-import socket
-def isConnectInternet():
-    try:
-        host = socket.gethostbyname("www.google.com")
-        s = socket.create_connection((host, 80), 2)
-        return True
-    except:
-        pass
-    return False
+import MySQLdb
+import json
+
+sql = "insert into raw_data "\
+       "(time,mos1,mos2,tmp1,tmp2,light_in,light_out)"\
+       "values ("1479296915","39.47", "42.57","29.21","\
+                "30.02","1280.12","4578.36");"
