3305070941a123834e0ed6b377544bd00d9ea812
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 19:21:58 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,7 +1,8 @@
 import MySQLdb
 db = db = MySQLdb.connect(host,username,password,database)
 cursor = db.cursor()
-sql = "insert into log_data (time,working,working_type,val_before,val_after)"\
+sql = "insert into log_data "\
+      "(time,working,working_type,val_before,val_after)"\
       "values (1479101649,1,1,19.65,36.27);"
 cursor.execute(sql)
 db.commit()
