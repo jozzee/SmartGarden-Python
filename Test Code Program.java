02850b9553bbaee194e421921864dfd3b9e6e44b
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 22:56:33 +0700

    Update and rename Test Code Program.py to Test Code Program.java

---
+++ Test Code Program.java
@@ -0,0 +1,5 @@
+ public boolean isConnectInternet(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected()) ? true : false;
+    }
