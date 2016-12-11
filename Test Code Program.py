d419eef9abb3f71ffbce167b6b5848d7159f3a14
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 20:44:37 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,9 +1,20 @@
-import socket
-def isConnectInternet():
-      try:
-            host = socket.gethostbyname("www.google.com")
-            s = socket.create_connection((host, 80), 2)
-            return True
-      except:
-            pass
-      return False
+import microgear.client as microgear
+import time
+microgear.create(<gearkey>,<gearsecret>, <appid>,{'debugmode': True})
+def connection():
+      print("Now I am connected with netpie")
+def subscription(topic,message):
+      print(topic+" "+message)
+def disconnect():
+      print("disconnected")
+microgear.setalias("Raspberry Pi Python")
+microgear.on_connect = connection
+microgear.on_message = subscription
+microgear.on_disconnect = disconnect
+microgear.subscribe("/token")
+microgear.subscribe("/refresh")
+microgear.subscribe("/settingStandard")
+microgear.subscribe("/settingDetails")
+microgear.subscribe("/controlDevieces")
+microgear.subscribe("/alarm")
+microgear.connect()
