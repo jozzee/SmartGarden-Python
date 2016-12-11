07510489f9bd5c4bea1904c7194f39508b302680
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 22:28:48 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -8,4 +8,4 @@ GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 def countPulse(channel):
     global count
     count = count+1
-GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)#FALLING
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)
