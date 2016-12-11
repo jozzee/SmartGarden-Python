670ea5f995504b8e5c1bdf38fdab8ee2c4884ad8
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 22:25:24 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,5 +1,11 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.output(29,True)
-time.sleep(10)
-GPIO.output(29,False)
+GPIO.setmode(GPIO.BOARD)
+FLOW_SENSOR = 37
+global count
+count = 0
+GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+def countPulse(channel):
+    global count
+    count = count+1
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)#FALLING
