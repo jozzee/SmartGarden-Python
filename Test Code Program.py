0c53a277b0e957a3e08c7f63791bea6e1c9aa822
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 19:19:09 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,18 +1,8 @@
 import MySQLdb
-data = "["
 db = db = MySQLdb.connect(host,username,password,database)
 cursor = db.cursor()
-cursor.execute("select * from raw_data order by time desc;")
-for i in range(cursor.rowcount):
-       row = cursor.fetchone()
-       list = "{\"time\":"+str(row[0])+","\
-              "\"mos1\":"+str(row[1])+",\"mos2\":"+str(row[2])\
-              ",\"tmp1\":"+str(row[3])+",\"tmp2\":"+str(row[4])\
-              ",\"light_in\":"+str(row[5])+","\
-              "\"light_out\":"+str(row[6])+"}"
-       if((i+1)==cursor.rowcount):
-              list+="]"
-       else:
-              list+=","
-       data+=list
+sql = "insert into log_data (time,working,working_type,val_before,val_after)"\
+      "values (1479101649,1,1,19.65,36.27);"
+cursor.execute(sql)
+db.commit()
 db.close()
