c0824f44c8166f40acf5885c8635c58e87778f63
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 19:43:31 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,9 +1,16 @@
 import MySQLdb
+data = "["
 db = db = MySQLdb.connect(host,username,password,database)
 cursor = db.cursor()
-sql = "insert into log_data "\
-      "(time,working,working_type,val_before,val_after)"\
-      "values (1479101649,1,1,19.65,36.27);"
-cursor.execute(sql)
-db.commit()
+cursor.execute("select * from log_data order by time desc;")
+for i in range(cursor.rowcount):
+      row = cursor.fetchone()
+      js = "{\"time\":"+str(row[0])+",\"working\":"+str(row[1])\
+      +",\"type\":"+str(row[2])+",\"val_berfore\":"+str(row[3])\
+      +",\"val_after\":"+str(row[4])+"}"
+      if((i+1)==cursor.rowcount):
+            js+="]"
+      else:
+            js+=","
+      data+=js
 db.close()
