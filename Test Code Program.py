0a9814710adacee533eed979f7daabe2513e4b6e
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 22:11:56 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,3 +1,5 @@
-import picamera
-def captureImage():
-      os.system("fswebcam -r 1280x720 --no-banner image/garden.jpg")
+import base64
+def cvIM2Base64():
+      with open("image/garden.jpg","rb") as imageFile:
+            imageAsString = base64.b64encode(imageFile.read())
+      return imageAsString.decode('utf-8')
