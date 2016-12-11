84da7a28b383bde9a33d21f9100125cdda3cbb95
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 23:05:18 +0700

    Update Test Code Program.java

--- Test Code Program.java
+++ Test Code Program.java
@@ -1,7 +1,1 @@
- public boolean isConnectInternet(Context context) {
-        ConnectivityManager connectMng = 
-         (ConnectivityManager)context
-          .getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo n = connectMng.getActiveNetworkInfo();
-        return (n != null && n.isConnected()) ? true : false;
-    }
+microgear.connect("AppId","Key","Secret","android");
