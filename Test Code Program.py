ede9eb5130117e61b796a3c42a4855ace8d3136a
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 18:17:04 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -5,9 +5,10 @@ cursor = db.cursor()
 cursor.execute("select * from raw_data order by time desc;")
 for i in range(cursor.rowcount):
        row = cursor.fetchone()
-       list = "{\"time\":"+str(row[0])+",\"mos1\":"+str(row[1])+","\
-              "\"mos2\":"+str(row[2])+",\"tmp1\":"+str(row[3])+","\
-              "\"tmp2\":"+str(row[4])+",\"light_in\":"+str(row[5])+","\
+       list = "{\"time\":"+str(row[0])+","\
+              "\"mos1\":"+str(row[1])+",\"mos2\":"+str(row[2])\
+              ",\"tmp1\":"+str(row[3])+",\"tmp2\":"+str(row[4])\
+              ",\"light_in\":"+str(row[5])+","\
               "\"light_out\":"+str(row[6])+"}"
        if((i+1)==cursor.rowcount):
               list+="]"
