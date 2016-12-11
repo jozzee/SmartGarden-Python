08c5f2fa5717a2d28f111fa03329094e4c6bc17b
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 22:57:14 +0700

    Update Test Code Program.java

--- Test Code Program.java
+++ Test Code Program.java
@@ -1,5 +1,6 @@
  public boolean isConnectInternet(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) 
+         context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnected()) ? true : false;
     }
