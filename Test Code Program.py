d0909ce09003fddc6b575ec7383632902f69770f
Jozziga <jozzeezaadee@gmail.com>
2016-12-11 20:31:39 +0700

    Update Test Code Program.py

--- Test Code Program.py
+++ Test Code Program.py
@@ -1,16 +1,9 @@
-import MySQLdb
-data = "["
-db = db = MySQLdb.connect(host,username,password,database)
-cursor = db.cursor()
-cursor.execute("select * from log_data order by time desc;")
-for i in range(cursor.rowcount):
-      row = cursor.fetchone()
-      js = "{\"time\":"+str(row[0])+",\"working\":"+str(row[1])\
-      +",\"type\":"+str(row[2])+",\"val_berfore\":"+str(row[3])\
-      +",\"val_after\":"+str(row[4])+"}"
-      if((i+1)==cursor.rowcount):
-            js+="]"
-      else:
-            js+=","
-      data+=js
-db.close()
+import socket
+def isConnectInternet():
+      try:
+            host = socket.gethostbyname("www.google.com")
+            s = socket.create_connection((host, 80), 2)
+            return True
+      except:
+            pass
+      return False
