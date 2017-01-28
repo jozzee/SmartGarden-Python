21b19a6207555822332fd520d199f5830e897113
Jozzee <sitthiphong.mun@gmail.com>
2017-01-28 14:08:30 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
-import datetime
 import time
-def isDay():
-    hour = datetime.datetime.now().strftime('%H')
-    if(int(hour)>6 and int(hour)<18):
-        return True
-    else:
-        return False
+import RPi.GPIO as GPIO
+
+GPIO.output(33,True)
+GPIO.output(35,False)
+time.sleep(1)
+GPIO.output(33,False)
+GPIO.output(35,False)
