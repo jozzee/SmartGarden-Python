4d277617a3e3de66e4ee6ce3995d97935e829ed5
Jozzee <sitthiphong.mun@gmail.com>
2017-01-28 19:04:48 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,3 @@
-import time
-import RPi.GPIO as GPIO
-
-GPIO.output(33,True)
-GPIO.output(35,False)
-time.sleep(1)
-GPIO.output(33,False)
-GPIO.output(35,False)
+from gcm import *
+token = "xgjknb;fbg...."
+gcm.plaintext_request(registration_id=token,data="message...")
