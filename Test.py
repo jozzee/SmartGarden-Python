71dd7a12b3fb9fb2b7ab5882630c0f00f2c625a1
Jozzee <sitthiphong.mun@gmail.com>
2017-02-03 23:05:20 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,9 @@
-from gcm import *
-token = "xgjknb;fbg...."
-gcm.plaintext_request(registration_id=token,data="message...")
+import json
+global obj
+def get(key):
+    getSharedPreferences()
+    if(obj != None):
+        for r in obj:
+            if(key in r):
+                return obj[key]
+fqPData = get("fqPData")              
